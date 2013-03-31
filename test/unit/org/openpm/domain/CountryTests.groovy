36633eb19a354eb99a8a957df140527faaeaae94
booking59 <ouzouighkamel@gmail.com>
2013-03-31 18:52:37 +0200

    first commit

---
+++ test/unit/org/openpm/domain/CountryTests.groovy
@@ -0,0 +1,15 @@
+package org.openpm.domain
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Country)
+class CountryTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
