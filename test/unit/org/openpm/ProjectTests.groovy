13fa702d58d5811254eb3ddad0e10ee96d976011
booking59 <ouzouighkamel@gmail.com>
2013-04-01 20:12:05 +0200

    first commit

---
+++ test/unit/org/openpm/ProjectTests.groovy
@@ -0,0 +1,18 @@
+package org.openpm
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+import org.openpm.Project;
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Project)
+class ProjectTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
