923a5a7f18fbd2668c1d5690fd443b2aba9b531c
booking59 <ouzouighkamel@gmail.com>
2013-04-09 23:23:18 +0200

    adding country

--- grails-app/domain/org/openpm/User.groovy
+++ grails-app/domain/org/openpm/User.groovy
@@ -4,9 +4,11 @@ class User {
 	
 	String email
 	String password
+	Country country
 	
     static constraints = {
 		email email:true, blank:false, nullable:false, unique:true
 		password blank:false, nullable: false, size:5..15
+		country nullable: false
     }
 }
