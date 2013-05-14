4c784decc8b6e71c6f4612db942b4b96e66e5c19
booking59 <ouzouighkamel@gmail.com>
2013-05-14 23:34:30 +0200

    writing validation tests

--- test/unit/org/openpm/ProjectTests.groovy
+++ test/unit/org/openpm/ProjectTests.groovy
@@ -1,7 +1,5 @@
 package org.openpm
 
-
-
 import grails.test.mixin.*
 import org.junit.*
 import org.openpm.Project;
@@ -12,7 +10,71 @@ import org.openpm.Project;
 @TestFor(Project)
 class ProjectTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+    void setUp() {
+		mockForConstraintsTests(Project)
+	}
+	
+	void testNameNull() {
+		def project = new Project(name:null)
+		
+		assert !project.validate()
+		
+		assert 'nullable' == project.errors['name']
+	}
+	
+	void testNameBlank() {
+		def project = new Project(name:'')
+		
+		assert !project.validate()
+		
+		assert 'blank' == project.errors['name']
+	}
+	
+	void testNameLengthLessThan4() {
+		def project = new Project(name:'prj')
+		
+		assert !project.validate()
+		
+		assert 'minSize' == project.errors['name']
+	}
+	
+	void testCodeNull() {
+		def project = new Project(code:null)
+		
+		assert !project.validate()
+		
+		assert 'nullable' == project.errors['code']
+	}
+	
+	void testCodeBlank() {
+		def project = new Project(code:'')
+		
+		assert !project.validate()
+		
+		assert 'blank' == project.errors['code']
+	}
+	
+	void testCodeLengthLessThan3() {
+		def project = new Project(code:'pr')
+		
+		assert !project.validate()
+		
+		assert 'minSize' == project.errors['code']
+	}
+	
+	void testClientNull() {
+		def project = new Project(client:null)
+		
+		assert !project.validate()
+		
+		assert 'nullable' == project.errors['client']
+	}
+	
+	void testCountriesEmpty() {
+		def project = new Project(countries:[])
+		
+		assert !project.validate()
+		
+		assert 'minSize' == project.errors['countries']
+	}
 }
