f9486883ee2a255af26c820e29f570c8a29f0652
booking59 <ouzouighkamel@gmail.com>
2013-05-20 18:39:06 +0200

    prototyping client

---
+++ grails-app/controllers/org/openpm/ClientController.groovy
@@ -0,0 +1,24 @@
+package org.openpm
+
+class ClientController {
+
+	static defaultAction = "list"
+	
+    def list() {
+		def clients = []
+		clients.add(new Client(name:'Google', contactName:'Google customer', email:'admin@google.com'))
+		clients.add(new Client(name:'Microsoft', contactName:'Microsoft customer', email:'admin@microsoft.com'))
+		clients.add(new Client(name:'Zynga', contactName:'Zynga customer', email:'admin@zynga.com'))
+		clients.add(new Client(name:'Yahoo', contactName:'Dupont customer', email:'admin@yahoo.com'))
+		clients.add(new Client(name:'VM Ware', contactName:'VM Ware customer',  email:'admin@vmware.com'))
+		clients.add(new Client(name:'Oracle', contactName:'Oracle customer', email:'admin@oracle.com'))
+		clients.add(new Client(name:'10 gen', contactName:'10gen customer', email:'admin@10gen.com'))
+		clients.add(new Client(name:'JBoss', contactName:'JBoss customer', email:'admin@jboss.org'))
+		
+		[clients: clients]
+	}
+	
+	def create() {
+		
+	}
+}
---
+++ grails-app/views/client/list.gsp
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<meta name="layout" content="main" />
+</head>
+<body>
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Contact name</th>
+			<th>Email</th>
+			<th>Country</th>
+			<th></th>
+		</tr>
+	</thead>
+	<g:each in="${clients}" var="client">
+		<tr>
+			<td>${client.name}</td>
+			<td>${client.contactName}</td>
+			<td>${client.email}</td>
+			<td>France</td>
+			<td>
+				<a class="btn btn-small btn-warning table-edit"><i class="icon-edit icon-white"></i></a>
+				<a class="btn btn-small btn-danger"><i class="icon-remove icon-white"></i></a>
+			</td>
+		</tr>
+	</g:each>
+</table>
+	<g:link controller="client" action="create" class="btn btn-primary"><i class="icon-plus icon-white"></i> Add new client</g:link>
+</body>
+</html>
