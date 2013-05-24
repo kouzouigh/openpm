fabfd4006c9008f9f7b21f4fa0e3318f243a8843
booking59 <ouzouighkamel@gmail.com>
2013-05-25 01:33:50 +0200

    adding countries fixture

---
+++ fixtures/countries.groovy
@@ -0,0 +1,13 @@
+import org.openpm.Country;
+
+fixture {
+	build {
+		algeria(Country, name: 'Algeria')
+		belgium(Country, name:'Belgium')
+		france(Country, name: 'France')
+		germany(Country, name: 'Germany')
+		spain(Country, name: 'Spain')
+		uk(Country, name: 'United Kingdom')
+		
+	}	
+}
