6c58ca55a5559b4b37cfaf2b7c1eb98b4315e204
booking59 <ouzouighkamel@gmail.com>
2013-05-29 23:48:14 +0200

    adding delete action

--- grails-app/controllers/org/openpm/ClientController.groovy
+++ grails-app/controllers/org/openpm/ClientController.groovy
@@ -5,16 +5,6 @@ class ClientController {
 	static defaultAction = "list"
 	
     def list() {
-		def clients = []
-		clients.add(new Client(id:1, name:'Google', contactName:'Google customer', email:'admin@google.com'))
-		clients.add(new Client(id:2, name:'Microsoft', contactName:'Microsoft customer', email:'admin@microsoft.com'))
-		clients.add(new Client(id:3, name:'Zynga', contactName:'Zynga customer', email:'admin@zynga.com'))
-		clients.add(new Client(id:4, name:'Yahoo', contactName:'Dupont customer', email:'admin@yahoo.com'))
-		clients.add(new Client(id:5, name:'VM Ware', contactName:'VM Ware customer',  email:'admin@vmware.com'))
-		clients.add(new Client(id:6, name:'Oracle', contactName:'Oracle customer', email:'admin@oracle.com'))
-		clients.add(new Client(id:7, name:'10 gen', contactName:'10gen customer', email:'admin@10gen.com'))
-		clients.add(new Client(id:8, name:'JBoss', contactName:'JBoss customer', email:'admin@jboss.org'))
-		
 		[clients: Client.findAll() ]
 	}
 	
@@ -53,5 +43,21 @@ class ClientController {
 				redirect(action:'list')
 			}
 		}	
+	}
+	
+	def deleteConfirmation() {
+		def clientInstance = Client.get(params.id)
+		println clientInstance
+		render template: '/common/deleteModal', 
+			   model: [ clientInstance: clientInstance,
+				   	   'headerTitle': 'Deleting client', 
+				   	   'bodyContent': "Are you sure about to delete client <b>${clientInstance.name}</b>"]
+	}
+	
+	def delete() {
+		def clientInstance = Client.get(params.id)
+		if( clientInstance ) {
+			Client.delete()
+		}
 	}
 }
