106e64e2a9e3a3cb9ae8e323f80e78dd42587f21
booking59 <ouzouighkamel@gmail.com>
2013-06-02 00:24:48 +0200

    adding constraint test

--- test/unit/org/openpm/InvoiceTests.groovy
+++ test/unit/org/openpm/InvoiceTests.groovy
@@ -9,7 +9,47 @@ import org.junit.*
 @TestFor(Invoice)
 class InvoiceTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+	void setUp() {
+		mockForConstraintsTests(Invoice)
+	}
+	
+	void testNullName() {
+		def invoice = new Invoice()
+		
+		assert !invoice.validate()
+	
+		assert 'nullable' == invoice.errors['name']
+	}
+	
+	void testNullBlank() {
+		def invoice = new Invoice(name:"")
+		
+		assert !invoice.validate()
+	
+		assert 'blank' == invoice.errors['name']
+	}
+	
+	void testClientNull() {
+		def invoice = new Invoice(client: null)
+		
+		assert !invoice.validate()
+	
+		assert 'nullable' == invoice.errors['client']
+	}
+	
+	void testProjectNull() {
+		def invoice = new Invoice(project: null)
+		
+		assert !invoice.validate()
+	
+		assert 'nullable' == invoice.errors['project']
+	}
+	
+	void testStatusNull() {
+		def invoice = new Invoice(status: null)
+		
+		assert !invoice.validate()
+	
+		assert 'nullable' == invoice.errors['status']
+	}
 }
