367f601ed3ab870206c81c3091c8cb7f7b6f1bfc
booking59 <ouzouighkamel@gmail.com>
2013-06-02 00:31:42 +0200

    pulling attributes

--- grails-app/domain/org/openpm/Invoice.groovy
+++ grails-app/domain/org/openpm/Invoice.groovy
@@ -1,7 +1,21 @@
 package org.openpm
 
 class Invoice {
+	
+	enum Status {
+		Todo,
+		Sent,
+		Paid,
+		Outstranding
+	}
+	
+	String name
+	Client client
+	Status status
+	Project project
+	InvoiceEvents events
 
     static constraints = {
+		name blank:false
     }
 }
