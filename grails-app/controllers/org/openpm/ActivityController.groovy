bb24c57daafe92dc53b6d47df0ed38f9fe5430bc
booking59 <ouzouighkamel@gmail.com>
2013-06-16 23:05:14 +0200

    loading consultants in create action

--- grails-app/controllers/org/openpm/ActivityController.groovy
+++ grails-app/controllers/org/openpm/ActivityController.groovy
@@ -4,10 +4,13 @@ class ActivityController {
 	
 	static defaultAction = "list"
 	
+	def userService
+	
     def list() { }
 	
 	def create() {
 		def activityInstance = new Activity();
-		[activityInstance: activityInstance]
+		def consultants = userService.findByRoleName("CONSULTANT") 
+		[activityInstance: activityInstance, consultants: consultants]
 	}
 }
