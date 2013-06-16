bd13425892d3b2270b4a0a56d594ba4c0d361f02
booking59 <ouzouighkamel@gmail.com>
2013-06-16 22:34:47 +0200

    default action is list

--- grails-app/controllers/org/openpm/ActivityController.groovy
+++ grails-app/controllers/org/openpm/ActivityController.groovy
@@ -1,7 +1,13 @@
 package org.openpm
 
 class ActivityController {
-
+	
+	static defaultAction = "list"
 	
     def list() { }
+	
+	def create() {
+		def activityInstance = new Activity();
+		[activityInstance: activityInstance]
+	}
 }
