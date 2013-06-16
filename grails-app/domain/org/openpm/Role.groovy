0ceb24beba5ac6b0f1991842717853901a66054a
booking59 <ouzouighkamel@gmail.com>
2013-06-16 12:08:09 +0200

    Overriding toString()

--- grails-app/domain/org/openpm/Role.groovy
+++ grails-app/domain/org/openpm/Role.groovy
@@ -6,7 +6,16 @@ class Role {
 	static hasMany = [ users: User, permissions: String ]
 	static belongsTo = User
 	
+	static mapping = {
+		cache true
+	}
+	
     static constraints = {
 		name(nullable: false, blank: false, unique: true)
     }
+	
+	@Override
+	public String toString() {
+		return name
+	}
 }
