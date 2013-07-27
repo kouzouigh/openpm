baf98d2383313a3ad4cabe733195e1de65684a91
booking59 <ouzouighkamel@gmail.com>
2013-07-27 21:08:00 +0200

    testing edit action

--- test/unit/org/openpm/UserControllerSpec.groovy
+++ test/unit/org/openpm/UserControllerSpec.groovy
@@ -7,13 +7,29 @@ import spock.lang.Specification
 @TestFor(UserController)
 class UserControllerSpec extends Specification {
 
-	void "Test edit with no id"() {
-		when:"A GET request is issued with no id"
+	def "Test edit with no id"() {
+		setup:
 		request.method = "GET"
+		
+		when:"A request is issued with no id"
 		controller.edit()
 		
-		then:"The not found page is rendered"
+		then:
 		response.status == 404
+	}
+	
+	def "Test edit with valid user id"() {
+		setup: "an existing user"
+		userInstance.save(flush:true, validate:false)
+		
+		and:"his id is passed as parameter"
+		controller.params.id = userInstance.id
+		
+		expect:
+		controller.edit() == [userInstance:userInstance]
+		
+		where:
+		userInstance = new User(email:"kamel@site.com")
 	}
 	
 }
