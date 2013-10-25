0af6439812b175bcaf26791e9136f2bd60d4a50d
booking59 <ouzouighkamel@gmail.com>
2013-10-26 01:26:21 +0200

    Changing tomcat and hibernate plugin version see http://grails.org/doc/latest/guide/upgradingFromPreviousVersionsOfGrails.html (Changes to Core plugin versioning schemes and the Upgrade command)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+        runtime ':hibernate:3.6.10.2'
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
 		runtime ":shiro:1.1.4"
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
-        build ":tomcat:$grailsVersion"
+		build ":tomcat:7.0.42"
 
         runtime ":database-migration:1.3.2"
 		
