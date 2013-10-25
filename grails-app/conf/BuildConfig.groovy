ee03590d37f1cc8c53bd64f34ce41d0be301a4d2
booking59 <ouzouighkamel@gmail.com>
2013-10-26 01:21:45 +0200

    Using maven for dependency resolution instead of Ivy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,13 +11,15 @@ grails.project.source.level = 1.6
 //   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
 //]
 
+grails.project.dependency.resolver = "maven" // or ivy
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
